6c03ab2972fbf8e9bb16804f592a0e6a622b7d95
theZalmanian <thezalmanian@gmail.com>
2023-05-15 04:59:26 -0700

    Created Instructor

---
+++ ComplexModelBinding/Models/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplexModelBinding.Models
+{
+    /// <summary>
+    /// Represents an individual Instructor
+    /// </summary>
+    public class Instructor
+    {
+        /// <summary>
+        /// The Instructor's Primary Key
+        /// </summary>
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The Instructor's full legal name
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
